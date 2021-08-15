9049f9502f43d92d2467c3280cba8135fac4f326
mbroughani81 <gitmbroughani81@gmail.com>
2021-08-15 23:28:46 +0430

    test

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -4,10 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import server.controller.SocketManager;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        LogManager.getLogger(Main.class).info("server started");
+        LogManager.getLogger(Main.class).info("server started  " + Instant.now());
 
         SocketManager socketManager = new SocketManager();
         socketManager.start();
